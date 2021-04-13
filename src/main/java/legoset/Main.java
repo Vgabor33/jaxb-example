685cedb7b2f85bc910b7fd9a2f8ea357cfe59ebb
Veress Gábor <veressgabi33@gmail.com>
2021-04-13 12:48:40 +0200

    Created XML assembly in Main and modified Minifig class

--- src/main/java/legoset/Main.java
+++ src/main/java/legoset/Main.java
@@ -1,9 +1,39 @@
 package legoset;
 
+import jakarta.xml.bind.JAXBException;
+import jaxb.JAXBHelper;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Set;
+
 public class Main
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws MalformedURLException, JAXBException, FileNotFoundException {
+        var legoset = new LegoSet();
+        legoset.setNumber(75211);
+        legoset.setUrl(new URL("https://brickset.com/sets/75211-1/Imperial-TIE-Fighter"));
+        legoset.setName("Imperial TIE Fighter");
+        legoset.setTheme("Star Wars");
+        legoset.setSubtheme("Solo");
+        legoset.setYear(Year.of(2018));
+        legoset.setPieces(519);
+        legoset.setPackaging(Packaging.BOX);
 
+        legoset.setTags(Set.of("Starfighter", "Stormtrooper", "Star Wars", "Solo"));
+        var minifigs = new ArrayList<Minifig>();
+        minifigs.add(new Minifig(2, "Imperial Mudtrooper" ));
+        minifigs.add(new Minifig(1, "Imperial Pilot"));
+        minifigs.add(new Minifig(1, "Mimban Stormtrooper"));
+
+        legoset.setMinifigs(minifigs);
+
+        legoset.setRatings(new Ratings(468,4.4));
+        //JAXBHelper.toXML(legoset,System.out);
+        JAXBHelper.toXML(legoset,new FileOutputStream("legoset.xml"));
     }
 }
--- src/main/java/legoset/Minifig.java
+++ src/main/java/legoset/Minifig.java
@@ -3,6 +3,7 @@ package legoset;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,5 +17,6 @@ public class Minifig
     @XmlAttribute
     private int count;
 
+    @XmlValue
     private String name;
 }
